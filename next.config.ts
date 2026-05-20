8096211c2d114e5e004aa0f178d016a5614c3dec
f4-fksu <eriksoto7725uyy@outlook.my>
2026-05-20 23:59:53 +0700

    feat:add data

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Use standalone output for server deployment (PM2/Docker)
-  output: "standalone",
+  /* config options here */
 };
 
 export default nextConfig;
